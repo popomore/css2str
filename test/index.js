918adcb9bf003a2279b4e901ed598e64d45d9510
popomore <sakura9515@gmail.com>
2014-07-03 18:55:24 +0800

    upgrade css@2.0.0 fix #1

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var should = require('should');
+require('should');
 var fs = require('fs');
 var join = require('path').join;
 var css2str = require('..');
@@ -25,5 +25,15 @@ describe('css2str', function() {
     var code = fs.readFileSync(join(base, 'a.css'));
     var expected = fs.readFileSync(join(base, 'a2.expect.css')).toString().slice(0, -1);
     css2str(code, {prefix: '.container'}).should.eql(expected);
+  });
+
+  it('should pass for comment after selector', function() {
+    var code = css2str('html input[type="button"], /* 1 */\ninput[type="reset"] {color:red;}');
+    code.should.eql('html input[type="button"],input[type="reset"]{color:red;}');
+  });
+
+  it('should support ie hack', function() {
+    var code = fs.readFileSync(join(base, 'iehack.css'));
+    css2str(code).should.eql('body{_padding:0;*padding:0;padding:0\\\\0;padding:0\\\\0/;padding:0\\\\9;}');
   });
 });
